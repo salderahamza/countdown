c255ac0c5e740afbc2259f79289b48a2c49998af
dev-zarl <hamzasafdark@gmail.com>
2020-09-07 20:26:50 +0500

    date manipulaton with javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputContainer = document.getElementById("input-container");
+const countdownForm = document.getElementById("countdownForm");
+const dateEl = document.getElementById("date-picker");
+
+const today = new Date().toISOString().split("T")[0];
+dateEl.setAttribute("min", today);
